03f9d1c01c3bc0b7e5c3b381c26a73519be2f9e2
Tracie Masek <traciemasek@gmail.com>
2018-11-12 23:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidayTo (holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
